ec3984b1570d8bab0ae436f40b366c27562f2a22
Brent_Dardenne <s109305@ap.be>
2020-10-27 22:38:01 +0100

    create header component and Create index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,104 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
+import {Wrapper, Image, BottomEdgeDown , Artist, BottomEdgeUp} from "./pageStyles/pageStyles"
+import { COLORS } from "../constants"
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  const {
+    wpcontent: {
+      page: {
+        homeMeta: {
+          homePageDescription,
+          homePageFeaturedArtists,
+          homePageHeaderDescription,
+          homePageHeaderPicture,
+          homePageHeaderTitle,
+        }
+      }
+    }
+  } = useStaticQuery(graphql`
+  query {
+    wpcontent {
+    page(id: "home", idType: URI) {
+      homeMeta {
+        homePageHeaderDescription
+        homePageHeaderDescription
+        homePageHeaderTitle
+        homePageHeaderPicture{
+          altText
+          sourceUrl
+          imageFile {
+            childImageSharp{
+              fluid(quality: 100) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+        homePageFeaturedArtists {
+          ... on WPGraphql_Artist {
+            id
+            artist {
+              artistName
+              firstName
+              lastName
+              profile{
+                altText
+                sourceUrl
+                  imageFile {
+                    childImageSharp{
+                      fluid(quality: 100, grayscale: true) {
+                        ...GatsbyImageSharpFluid_withWebp
+                      }
+                    }
+                  }
+                }
+              }
+            }  
+          }
+        }
+      }
+    }
+  }
+  `);
+  //console.log(homePageFeatureArtist)
+  //console.log(data)
+  return <Layout>
     <SEO title="Home" />
+    <Wrapper>
+      <div className="banner">
+        <Image fluid={homePageHeaderPicture.imageFile.childImageSharp.fluid} alt={homePageHeaderPicture.altText} 
+        />
+      
+      <div className="inner-div">
+        <p className="header-title">{homePageHeaderTitle}</p>
+        <p className="header-description">{homePageHeaderDescription}</p>
+     </div> 
+      <BottomEdgeDown color={COLORS.BLACK}/>
+      </div>
+      <div className="description">
+        <p>{homePageDescription}</p>
+        <BottomEdgeUp color={COLORS.PRIMARY} />
+      </div>
+      <div className="artists">
+        <h2>Featured Artists</h2>
+        <div className="artist-items">
+          {homePageFeaturedArtists.map(({artist, slug}) => (
+            <Artist to={`/${slug}`}>
+              <Image fluid={artist.profile.imageFile.childImageSharp.fluid} alsText={artist.profile.altText}/>
+              <div className="artist-info">
+                <p>{artist.first} {artist.lastName}</p>
+                <p>{artist.artistName}</p>
+              </div>
+            </Artist>
+          ))}
+        </div>
+      </div>
+    </Wrapper>
   </Layout>
-)
+}
 
 export default IndexPage
